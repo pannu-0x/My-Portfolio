c610aa409c3d62b61b63048804592946fad0534b
pannu-0x <pranayjadhao22@gmail.com>
2025-08-26 16:50:21 +0530

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".multiple-text", {
+    strings: ["Fronted Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
